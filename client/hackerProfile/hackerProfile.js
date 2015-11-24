cc1913fe204c1f80d3b7c1680975cc3dae7320a8
Alex Hoffnung <alex.hoffnung@gmail.com>
2015-11-24 18:51:16 -0500

    Add Hackers mongo collection

---
+++ client/hackerProfile/hackerProfile.js
@@ -0,0 +1,23 @@
+Template.hackerProfile.events {
+	"click .btn": function () {
+      let hacker = {};
+      hacker.peopleName = $("input#people-name").val() || "";
+      hacker.userEmail = $("input#user-email").val() || "";
+      hacker.userName = $("input#user-name").val() || "";
+      hacker.userHack = $("input#user-hack").val() || "";
+    
+      if( hacker.userEmail.length() === 0 ) return;
+      
+      Hackers.update(
+      	{email: hacker.userEmail}, 
+      	{$set: 
+ 			{
+ 				peopleName: hacker.peopleName,
+ 				userName: hacker.userName,
+ 				userHack: hacker.userHack
+ 			}
+      	},
+      	{ upsert: true }
+      );
+    }
+}
